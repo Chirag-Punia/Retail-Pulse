0de814267e14ed710422b9c175a3109e15752764
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2025-01-24 01:21:54 +0530

    Added CORS middleware to enable cross-origin requests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,3 +25,17 @@ func main() {
 	log.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
+func withCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
